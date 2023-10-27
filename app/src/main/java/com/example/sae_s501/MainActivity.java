46fa87395fe6ffd154d98cdae360c564b4c34afb
Baptistet21 <102140472+Baptistet21@users.noreply.github.com>
2023-10-27 12:20:22 +0200

    push de test

--- app/src/main/java/com/example/sae_s501/MainActivity.java
+++ app/src/main/java/com/example/sae_s501/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.sae_s501;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.gson.Gson;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +21,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setContentView(R.layout.connexion); // Charger le fichier "connexion.xml"
     }
+
+
 }
