1aab9a995d8aeae472bf981c2d90d8acec9118f2
AnselotSteven <Steven.Anselot@uphf.fr>
2023-11-07 10:43:09 +0100

    translation des strings.xml

--- app/src/main/java/com/example/sae_s501/MainActivity.java
+++ app/src/main/java/com/example/sae_s501/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.sae_s501;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.connexion); // Charger le fichier "connexion.xml"
+        setContentView(R.layout.connexion);
     }
 }
