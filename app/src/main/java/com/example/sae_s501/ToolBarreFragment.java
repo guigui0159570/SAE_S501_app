f9210b2b5983c90c36151c116e770919d6f6792f
Leveque Aurélien <Aurelien.Leveque@uphf.fr>
2023-11-20 12:06:32 +0100

    Creation compte + modification compte + affichage photo et modif + switch activité avec fragment animé + tool barre de navigation rapide

---
+++ app/src/main/java/com/example/sae_s501/ToolBarreFragment.java
@@ -0,0 +1,89 @@
+package com.example.sae_s501;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+
+import com.example.sae_s501.databinding.UpdatemoncompteBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ToolBarreFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ToolBarreFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ToolBarreFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ToolBarreFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ToolBarreFragment newInstance(String param1, String param2) {
+        ToolBarreFragment fragment = new ToolBarreFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_tool_barre, container, false);
+        ImageButton home = rootView.findViewById(R.id.homeBouton);
+        ImageButton profil = rootView.findViewById(R.id.profilBouton);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeview = new Intent(view.getContext(), MyUpdateCompteActivity.class); // a modifier avec la vrai page d'accueil
+                startActivity(homeview);
+            }
+        });
+
+        profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent profilview = new Intent(view.getContext(), MyCompteActivity.class);
+                startActivity(profilview);
+            }
+        });
+        return rootView;
+    }
+}
