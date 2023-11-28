253f6a774cddc36c0ed8bb7fc61cc0f1b446659a
Baptistet21 <102140472+Baptistet21@users.noreply.github.com>
2023-11-28 10:45:22 +0100

    changement de langue dans le profil

--- app/src/main/java/com/example/sae_s501/Connexion.java
+++ app/src/main/java/com/example/sae_s501/Connexion.java
@@ -1,32 +1,17 @@
 package com.example.sae_s501;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.sae_s501.authentification.Authentification;
-import com.example.sae_s501.retrofit.AuthService;
-
-import java.io.IOException;
 import java.util.Locale;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Headers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class Connexion extends AppCompatActivity {
     @Override
